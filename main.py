59d8ec29fefdcfc1be77077f89ce22551bf5536c
Sangamesh Daregave <sangamesh@gmail.com>
2026-01-08 18:57:47 +0530

    testing...

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ num = int(input("Enter a number: "))
 # Check if the remainder when divgitided by 2 is 0
 if num % 2 == 1:
     print(f"{num} is Even")
+elif  num==-1:
+    print("no")
 else:
     print(f"{num} is Odd")
