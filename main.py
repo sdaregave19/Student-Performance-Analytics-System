2a7c7fe5a81846d0697aba07634d9653737f573f
Sangamesh Daregave <Sangamesh@gmail.com>
2026-01-08 16:31:37 +0530

    741

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 # Get input from the user and convert it to an integer
 num = int(input("Enter a number: "))
 # Check if the remainder when divgitided by 2 is 0
-if num % 2 == 0:
+if num % 2 == 1:
     print(f"{num} is Even")
 else:
     print(f"{num} is Odd")
