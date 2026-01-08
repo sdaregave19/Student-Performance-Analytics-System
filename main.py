18d38a433af6d9cfb1c3a6aaae91253cdda026b6
Sangamesh Daregave <Sangamesh@gmail.com>
2026-01-08 16:17:32 +0530

    5

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-try:
-    # Get input from the user and convert it to an integer
-    num = int(input("Enter a number: "))
 
-    # Check if the remainder when divided by 2 is 0
-    if num % 2 == 0:
-        print(f"{num} is Even")
-    else:
-        print(f"{num} is Odd")
-except ValueError:
-    print("Please enter a vali.")
+# Get input from the user and convert it to an integer
+num = int(input("Enter a number: "))
+# Check if the remainder when divided by 2 is 0
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
